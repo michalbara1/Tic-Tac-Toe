b81b755fa914213108f39dc409b59a78b28e903b
Michal <michalbara1@gmail.com>
2024-12-06 15:14:14 +0200

    added winnig logic

--- app/src/main/java/com/example/tictactoe/GameActivity.kt
+++ app/src/main/java/com/example/tictactoe/GameActivity.kt
@@ -80,6 +80,38 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         GameData.saveGameModel(model)
     }
 
+    fun checkForWinner(){
+        val winningPos = arrayOf( //all the wining positions
+            intArrayOf(0,1,2),
+            intArrayOf(3,4,5),
+            intArrayOf(6,7,8),
+            intArrayOf(0,3,6),
+            intArrayOf(1,4,7),
+            intArrayOf(2,5,8),
+            intArrayOf(0,4,8),
+            intArrayOf(2,4,6),
+        )
+
+        gameModel?.apply {
+            for ( i in winningPos){
+                //012
+                if(
+                    filledPos[i[0]] == filledPos[i[1]] &&
+                    filledPos[i[1]]== filledPos[i[2]] &&
+                    filledPos[i[0]].isNotEmpty()
+                ){
+                    gameStatus = GameStatus.FINISHED
+                    winner = filledPos[i[0]]
+                }
+            }
+
+            if( filledPos.none(){ it.isEmpty() }){
+                gameStatus = GameStatus.FINISHED
+            }
+            updateGameData(this)
+        }
+    }
+
     override fun onClick(v: View?) {
         gameModel?.apply {
             if(gameStatus!= GameStatus.INPROGRESS){
